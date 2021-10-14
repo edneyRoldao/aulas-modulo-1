1d1152d656a7307d5ceea7acac2281a1fc7a1c1a
edneyRoldao <edneyroldao@gmail.com>
2021-10-14 05:48:33 -0300

    backup 01

---
+++ 02_variaveis-tp-dados.js
@@ -0,0 +1,30 @@
+// aula variável e tipos de dados
+/////////////////////////////////
+
+// var, let, const (vamos trabalhar só com let)
+
+// declarando uma variável (nome de variável de comecar com letra minuscula)
+let texto;
+
+// declarando mais de uma variavel
+let numero1, numero2, resultado;
+
+// definindo (atribuir) valor para uma variável
+texto = "esse e meu primeiro texto na programacao";
+numero1 = 10;
+numero2 = 20;
+resultado = false;
+
+console.log(texto);
+console.log(numero1);
+console.log(numero2);
+console.log(resultado);
+
+
+// declarando uma var e atribuindo ao mesmo tempo.
+let texto2 = "esse e a minha segunda variavel";
+
+console.log(texto2);
+
+
+let TEXTO = "outro texto";
---
+++ 03_operadores_matematicos.js
@@ -0,0 +1,51 @@
+// aula operadores matematicos
+
+// base dos exemplos
+let n1 = 10;
+let n2 = 20;
+let resultado;
+
+// adicao
+resultado = n1 + n2;
+console.log("exemplo adicao");
+console.log(resultado);
+
+// subtracao
+resultado = n2 - n1;
+console.log("exemplo subtracao");
+console.log(resultado);
+
+// multiplicacao
+resultado = n2 * n1;
+console.log("exemplo multiplicacao");
+console.log(resultado);
+
+
+// divisao
+resultado = n2 / n1;
+console.log("exemplo divisao");
+console.log(resultado);
+
+
+// exemplo resto da divisao
+// quando a divisao nao for exato, retorna 1, exp 25/2 = 12.5
+// quando divisao exato, retorna 0, exp 20/2 = 10.0
+n1 = 20;
+n2 = 2;
+resultado = n1 % n2;
+console.log("exemplo resto da divisao");
+console.log(resultado);
+
+// Operadores de incremento (++) e decremento (--)
+n1 = 10;
+resultado = ++n1;
+console.log("exemplo incremento");
+console.log(resultado);
+console.log(n1);
+
+// Operadores de incremento (++) e decremento (--)
+n1 = 10;
+resultado = --n1;
+console.log("exemplo decremento");
+console.log(resultado);
+console.log(n1);
